031b4e6e02c0ee43f765b821c037143e250d8459
mapoeeiphyu20 <mapoeeiphyu2017.miitinternship@gmail.com>
2020-01-11 23:38:23 +0630

    Modifying app-routing.module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,36 +16,36 @@ const routes: Routes = [
     loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule)
   },
   {
+    path: 'video',
+    loadChildren: () => import('./video/video.module').then(m => m.VideoPageModule)
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('./article/article.module').then(m => m.PaymentPageModule)
+  },
+  {
+    path: 'course',
+    loadChildren: () => import('./course/course.module').then(m => m.CoursePageModule)
+  },
+  {
+    path: 'lecturer-profile',
+    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
+  },
+  {
+    path: 'student-voice',
+    loadChildren: () => import('./student-voice/student-voice.module').then(m => m.StudentVoicePageModule)
+  },
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then(m => m.ShopPageModule)
+  },
+  {
+    path: 'event',
+    loadChildren: () => import('./event/event.module').then(m => m.EventPageModule)
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
-  },
-  {
-    path: 'payment',
-    loadChildren: () => import('./payment/payment.module').then(m => m.PaymentPageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
-  },
-  {
-    path: 'lecturer-profile',
-    loadChildren: () => import('./lecturer-profile/lecturer-profile.module').then(m => m.LecturerProfilePageModule)
   }
 ];
 
