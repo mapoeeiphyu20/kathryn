57cc0699c536236b54c2759f500cc1f083de755c
mapoeeiphyu20 <mapoeeiphyu2017.miitinternship@gmail.com>
2020-01-15 00:55:04 +0630

    Json

--- src/providers/mock-events.ts
+++ src/providers/mock-events.ts
@@ -1,75 +1,53 @@
 let events: Array<any> = [
     {
         id: 1,
-        name: "Caroline Kingsley",
-        title: "Ethical Leadership",
+        name: "Kathryn Talks",
+        title: "စွမ်းရည်မြင့်မားသော လူငယ်ခေါင်းဆောင်များ ဖြစ်ထွန်းလာရေးအတွက် Yli",
         phone: "617-244-3672",
         mobilePhone: "617-244-3672",
         email: "caroline@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/caroline_kingsley.jpg"
+        picture: "/assets/img/kathryntalks.jpg",
+        description : "24 Feb 2018 at 9:00 A.M. to 5:00 P.M. Mandalay : London Business Academy - Mandalay"
     },
     {
         id: 2,
-        name: "Michael Jones",
-        title: "Women Leadership(Workshop)",
+        name: "Kathryn Talks",
+        title: "လူငယ်ခေါင်းဆောင်များ ပေါ်ထွန်းလာရေးအတွက် မြန်မာပြည်၏ ပထမဆုံး YLI Program (2)",
         phone: "617-244-3672",
         mobilePhone: "617-244-3672",
         email: "michael@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg"
+        picture: "/assets/img/kathryntalks.jpg",
+        description : "28 Apr 2018 at 9:00 A.M. to 5:00 P.M. Mandalay : London Business Academy - Mandalay"
     },
     {
         id: 3,
-        name: "Jonathan Bradley",
-        title: "Event 3",
+        name: "Kathryn Talks",
+        title: "Country's Future & Young Leaders (Free Event)",
         phone: "617-244-3672",
         mobilePhone: "617-244-3672",
         email: "jonathan@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg"
+        picture: "/assets/img/event3.jpg",
+        description : "24 Feb 2019 at 9:00 A.M. to 1:00 P.M. Mandalay : London Business Academy - Mandalay"
     },
     {
         id: 4,
-        name: "Jennifer Wu",
-        title: "Event 4",
+        name: "Kathryn Talks",
+        title: "Young Leaders Initiative Program (Batch 5)",
         phone: "617-244-3672",
         mobilePhone: "617-244-3672",
         email: "jen@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg"
+        picture: "/assets/img/event4.jpg",
+        description : "May 4, 2019 at 9 AM – May 5, 2019 at 5 PM Mandalay : London Business Academy - Mandalay"
     },
     {
         id: 5,
-        name: "Olivia Green",
-        title: "Event 5",
+        name: "Kathryn Talks",
+        title: "Young Leadership (Workshop) အမျိုးသမီး ခောင်းဆောင်မှု စွမ်းရည်မြင့်",
         phone: "617-244-3672",
         mobilePhone: "617-244-3672",
         email: "olivia@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg"
-    },
-    {
-        id: 6,
-        name: "Miriam Aupont",
-        title: "Event 6",
-        phone: "617-244-3672",
-        mobilePhone: "617-244-3672",
-        email: "miriam@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg"
-    },
-    {
-        id: 7,
-        name: "Michelle Lambert",
-        title: "Event 7",
-        phone: "617-244-3672",
-        mobilePhone: "617-244-3672",
-        email: "michelle@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg"
-    },
-    {
-        id: 8,
-        name: "Victor Ochoa",
-        title: "Event 8",
-        phone: "617-244-3672",
-        mobilePhone: "617-244-3672",
-        email: "victor@ionicrealty.com",
-        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg"
+        picture: "/assets/img/event5.jpg",
+        description : "August 11, 2019 at 10 AM to 4 PM Mandalay : London Business Academy - Mandalay"
     }
 ];
 
